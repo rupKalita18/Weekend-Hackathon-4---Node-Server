df651e9c878d5f4fb7d7cb63aa7b90160be7e292
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-11-11 08:47:41 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 var http = require("http");
 
-const httpServer = http.createServer(handleServer);
-
+const httpServer = http.createserver(handleServer);
 
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write(" Welcome to Dominos!");
+    res.end();
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        phone: "18602100000",
+        email: "guestcaredominos@jublfood.com"
+      })
+    );
+
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("404 Not Found");
+  }
 }
 
 module.exports = httpServer;
